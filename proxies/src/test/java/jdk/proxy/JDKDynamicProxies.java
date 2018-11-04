979e67513e772330e2eeba9310247c31aac1e697
Rohan Suri <rohan.a.suri@gmail.com>
2018-11-04 23:09:55 +0530

    added comment explaining how jdk creates proxies at runtime. It sets up the class file at runtime byte by byte

--- proxies/src/test/java/jdk/proxy/JDKDynamicProxies.java
+++ proxies/src/test/java/jdk/proxy/JDKDynamicProxies.java
@@ -3,6 +3,8 @@ package jdk.proxy;
 import jdk.proxy.AppContext;
 import jdk.proxy.TimeMe;
 import jdk.proxy.TimeMeImpl;
+import org.apache.bcel.ExceptionConst;
+import org.apache.bcel.classfile.ClassParser;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +12,18 @@ import org.slf4j.LoggerFactory;
 // JDK's Dynamic Proxy limitation:
 // can only proxy interface methods
 // for more power, we need to use CGLIB
+/*
+    see sun.misc.ProxyGenerator.ProxyMethod
+    JDK at runtime creates a class file and puts the bytes for the methods in the interfaces we supplied,
+    putting method signatures, etc
+    and code inside each of those methods is simply calling the invocationhandler's invoke method
+    with the right arguments
+ */
 public class JDKDynamicProxies {
     private static final Logger logger = LoggerFactory.getLogger(JDKDynamicProxies.class);
 
     @Test
-    public void test() throws InterruptedException {
+    public void test() throws Exception  {
         AppContext ctx = new AppContext();
 
         // before proxying
@@ -29,7 +38,7 @@ public class JDKDynamicProxies {
         // after proxying
         impl.timeMe();
 
-        logger.info("Runtime class is {}",impl.getClass());
+        logger.info("Runtime class is {}", impl.getClass());
 
     }
 
